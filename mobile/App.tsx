9b28786aec03ecbc06c6ea6360d6e7b396a120fd
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2020-10-27 00:40:54 -0300

    Exibição do mapa e rotas de navegação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,31 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+import { useFonts } from "expo-font";
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from "@expo-google-fonts/nunito";
+
+
+import Routes from './src/routes'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -14.2146996,
-          longitude: -39.5189474,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      />
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-});
