5efa0ac3cbbe48649e871793299ef962de39305d
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2020-10-26 23:26:50 -0300

    Versão mobile iniciada

---
+++ mobile/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -14.2146996,
+          longitude: -39.5189474,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
