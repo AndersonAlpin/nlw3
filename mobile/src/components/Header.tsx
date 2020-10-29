5764878c8098eec055549ac6815b6b9e25d73b62
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2020-10-29 00:21:18 -0300

    Criação do componente Header

---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { BordelessButton } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons"
+
+interface HeaderProps {
+  title: string;
+}
+
+export default function Header({ title }: HeaderProps) {
+  return (
+    <View style={styles.container}>
+      <BordelessButton onPress={() => {1}}>
+        <Feather name="arrow-left" size={24} color="#15b6d6" />
+      </BordelessButton>
+
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: "#f9fafc",
+    borderBottomWidth: 1,
+    borderColor: "#dde3f0",
+    paddingTop: 44,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  title: {
+    fontFamily: "Nunito_600SemiBold",
+    color: "#8fa7b3",
+    fontSize: 16,
+  },
+});
